2596d67027d7522dc4ef6e9862af8f4cf562437d
raulcontreraso-bit <raulcontreraso@gmail.com>
2026-07-04 20:39:24 +0200

    1

--- Learn Python.py
+++ Learn Python.py
@@ -633,7 +633,8 @@ def greeting(name,age=28):
     print("Hello " + name + ", you are " + str(age) + "!")
     print(f"Hello {name}, you are {age}!")
 
-#name = input("Enter your name: ")    
+name1 = input("Enter your name: ")  
+greeting(name1,32)  
 name = 'Luis'
 greeting(name,32)
 greeting("Judith")
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Welcome back to Unix and C!\n");
+    return 0;
+}
